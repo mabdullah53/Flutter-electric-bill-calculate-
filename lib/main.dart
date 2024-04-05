ce84d92efda8fd943cfc5ce62d3dd9f2dadf39cd
Muhammad Abdullah <165842713+mabdullah53@users.noreply.github.com>
2024-04-05 15:40:31 -0700

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Flutter',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
